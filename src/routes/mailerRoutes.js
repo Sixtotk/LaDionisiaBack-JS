fe7f78396f61222799d1d47c5ab6abf0033a4264
Guillermo Ojeda <gro1413@hotmail.com>
2023-01-13 16:27:58 -0300

    envio de correo basico funcionando

---
+++ src/routes/mailerRoutes.js
@@ -0,0 +1,26 @@
+const { Router } = require("express");
+const router = Router()
+
+const { transporter } = require("../mailer/mailer");
+
+
+
+router.post("/", async (req,res) => {
+    const { userEmail, userName } = req.body
+    try {
+        await transporter.sendMail({
+            from: "La Dionisia <ladionisia@gmail.com",
+            to: userEmail,
+            subject: "Suscripcion exitosa!",
+            html: `<h4> Mensaje para ${userEmail}</h4>
+            <p> si este email no te pertenece por favor ignoralo</p>
+            <h2> Hola ${userName}</h2>
+            Te damos las gracias por suscribirte a "La Dionsia"
+            `
+        })
+    } catch (error) {
+        console.log(error)
+    }
+} )
+
+module.exports = router;
