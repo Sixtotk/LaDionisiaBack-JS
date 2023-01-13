9171a2c764b85828de4151fb26b55ad0af8d108f
Guillermo Ojeda <gro1413@hotmail.com>
2023-01-13 18:57:50 -0300

    cambios menores en mailerRoutes

--- src/routes/mailerRoutes.js
+++ src/routes/mailerRoutes.js
@@ -9,7 +9,7 @@ router.post("/", async (req,res) => {
     const { userEmail, userName } = req.body
     try {
         await transporter.sendMail({
-            from: "La Dionisia <ladionisia@gmail.com",
+            from: "La Dionisia <ladionisia@gmail.com>",
             to: userEmail,
             subject: "Suscripcion exitosa!",
             html: `<h4> Mensaje para ${userEmail}</h4>
@@ -18,6 +18,8 @@ router.post("/", async (req,res) => {
             Te damos las gracias por suscribirte a "La Dionsia"
             `
         })
+
+        res.status(200).send("Email enviado")
     } catch (error) {
         console.log(error)
     }
