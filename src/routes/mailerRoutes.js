9c6ad550dc57dff7f1840b612a841b442d2944b5
Guillermo Ojeda <gro1413@hotmail.com>
2023-01-16 14:19:03 -0300

    mail configurado para compra y suscripcion

--- src/routes/mailerRoutes.js
+++ src/routes/mailerRoutes.js
@@ -6,20 +6,34 @@ const { transporter } = require("../mailer/mailer");
 
 
 router.post("/", async (req,res) => {
-    const { userEmail, userName } = req.body
+    const { userEmail, newsletter } = req.body
+
     try {
+
+    if(newsletter){
         await transporter.sendMail({
             from: "La Dionisia <ladionisia@gmail.com>",
             to: userEmail,
             subject: "Suscripcion exitosa!",
             html: `<h4> Mensaje para ${userEmail}</h4>
             <p> si este email no te pertenece por favor ignoralo</p>
-            <h2> Hola ${userName}</h2>
+                            <h2> ¡Hola!</h2>
             Te damos las gracias por suscribirte a "La Dionsia"
             `
         })
-
-        res.status(200).send("Email enviado")
+        res.status(200).send("Email de suscripcion enviado")
+        
+    } else {
+        await transporter.sendMail({
+            from: "La Dionisia <ladionisia@gmail.com>",
+            to: userEmail,
+            subject: "Compra realizada!",
+            html: `
+            <h4> ¡Tu compra ha sido realizada con exito!</h4>
+            `
+        })
+        res.status(200).send("Compra realizada")
+    }
     } catch (error) {
         console.log(error)
     }
