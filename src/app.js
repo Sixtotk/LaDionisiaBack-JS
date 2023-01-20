baf6ab3fd62aebb25045f36e7710962a59f13b13
Sixtotk <sixtoorden@gmail.com>
2023-01-20 16:32:39 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -10,33 +10,12 @@ require('./db.js');
 
 const server = express();
 
-// const whiteList = [
-//   "https://la-dionisia-front.vercel.app",
-//   "http://localhost:3000"
-// ]
-
-// server.name = 'API';
-// const corsConfig = {
-//   origin: function(origin, callback){
-//     if(whiteList.indexOf(origin) !== -1 || !origin){
-//       callback(null, true);
-//     } else {
-//       callback(new Error(" Not Allowed by CORS"));
-//     }
-//   },
-//   credentials: true,
-//   methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-//   allowedHeaders: ['Content-Type']
-// };
-
-// server.use(cors(corsConfig));
-// server.use(cors({ origin: whiteList}));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin','http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin','*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE');
