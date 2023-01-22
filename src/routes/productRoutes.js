b61ec2acb43a957fa4337d5a2610b69487d5c940
elvis huanca <JHuanca_21@outlook.com>
2023-01-21 23:02:31 -0500

    route delete product

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,20 +1,21 @@
 const { Router } = require("express");
-const { getAllProducts, getProductsById, disableProduct, postProduct, updateProduct, getDisabledProducts, getFeatured, getBestRated, getWineByYear, getWineTypes, getWineByCountry, getWineByRegion} = require("../controllers/productControllers");
+const { getAllProducts, getProductsById, disableProduct, postProduct, updateProduct, getDisabledProducts, getFeatured, getBestRated, getWineByYear, getWineTypes, getWineByCountry, getWineByRegion, deleteProduct } = require("../controllers/productControllers");
 
 const router = Router();
 
 router.get('/', getAllProducts)
-router.get('/disabled',getDisabledProducts)
-router.get('/featured',getFeatured)
-router.get('/wineTypes/:type',getWineTypes)
-router.get('/wineByYear',getWineByYear)
-router.get('/region/:region',getWineByRegion)
-router.get('/country/:country',getWineByCountry)
-router.get('/:id',getProductsById)
-router.get('/bestRated',getBestRated)
-router.patch('/disable/:productId',disableProduct)
+router.get('/disabled', getDisabledProducts)
+router.get('/featured', getFeatured)
+router.get('/wineTypes/:type', getWineTypes)
+router.get('/wineByYear', getWineByYear)
+router.get('/region/:region', getWineByRegion)
+router.get('/country/:country', getWineByCountry)
+router.get('/:id', getProductsById)
+router.get('/bestRated', getBestRated)
+router.patch('/disable/:productId', disableProduct)
+router.delete('/delete/:productId', deleteProduct)
 router.post('/', postProduct)
-router.put('/:id',updateProduct)
+router.put('/:id', updateProduct)
 
-  // .delete(archiveProduct);
+// .delete(archiveProduct);
 module.exports = router;
